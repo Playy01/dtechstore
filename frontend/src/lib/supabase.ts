631256504d649b1ef6ffd596615dec7ebc85e056
Angel Daniel Olivares Cruz <angeldanielolivarescruz@gmail.com>
2025-12-01 11:37:33 -0600

    Fix: Corregir URL de Supabase

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL || 'https://szfamthbjrsqctzgokx.supabase.co';
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL || 'https://szfwntkbjrspcptzgoks.supabase.co';
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6ZndudGtianJzcGNwdHpnb2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MzcxODEsImV4cCI6MjA4MDExMzE4MX0.RHng44u3pPpUZwlb-W1KvoyCi7RTVNtmw7S7LjgG0G0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
